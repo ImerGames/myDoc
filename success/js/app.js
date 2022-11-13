5de5c5e9852e0f5bbe61999b201348bc585a2aa9
ZiroxCh <40709454+ImerGames@users.noreply.github.com>
2022-11-13 22:21:19 +0200

    Add files via upload

--- success/js/app.js
+++ success/js/app.js
@@ -1,22 +1,78 @@
-$(document).ready(function(){
-    $("#close_burger_menu").click(function(){
-        $("header>.container>nav").css({left:"-100%"});
-        $("header>.container>nav").fadeOut(300);
-    });
-    $("#open_burger_menu").click(function(){
-        $("header>.container>nav").fadeIn(300);
-        $("header>.container>nav").css({left:"0" });
-    });
-    $("#open__more").click(function(){
-        const is_open = $(this).attr("data-open");
-        if(is_open == "true"){
-            $(".grid__container__program > .hide__element").fadeIn(300);
-            $(this).attr("data-open" , "false");
-            $(this).text("Показать менше");
-        }else{
-            $(".grid__container__program > .hide__element").fadeOut(300);
-            $(this).attr("data-open" , "true");
-            $(this).text("Показать еше");
+$(".btn-close-menu-mob").click(function(){
+    $(".burger-list").css("left","-50%");
+});
+$(".button_open_burger").click(function(){
+    $(".burger-list").css("left","0px");
+});
+
+function init_swiper_cards(){
+    const swiper_001 = new Swiper('.swiper_init_001', {
+        loop: false,
+        slidesPerView: 3,
+        breakpoints: {
+            376: {
+              slidesPerView: 2,
+              centeredSlides: true,
+            },
+            1200: {
+                slidesPerView: 3,
+                centeredSlides: false,
+            }
         }
     });
-});
+
+    const swiper_002 = new Swiper(".swiper_init_002", {
+        loop: false,
+        slidesPerView: 3,
+        grid: {
+            rows: 2,
+        },
+        navigation: {
+            prevEl: "#swiper_init_002_left",
+            nextEl: "#swiper_init_002_right"
+        }
+    });
+
+    const swiper_003 = new Swiper(".swiper_init_003", {
+        loop: false,
+        slidesPerView: 1,
+        spaceBetween:30,
+        navigation: {
+            prevEl: "#swiper_init_003_left",
+            nextEl: "#swiper_init_003_right"
+        }
+    });
+}
+
+function init_select(){
+    const object = $(".select-init");
+
+    $(object).click(function(){
+        let isOpen = $(this).children(".list").is(":hidden");
+
+        if(isOpen){
+            $(this).children(".list").show(300);
+        }else{
+            $(this).children(".list").hide(300);
+        }
+    });
+
+    $(object).find("li").click(function(){
+        let text = $(this).text();
+        $(this).closest(".select-init").find(".display").children(".title").text(text);
+        let obj = $(this);
+        setTimeout(function(){
+            $(obj).parent().children("li").removeClass("active");
+            $(obj).addClass("active");
+        },300);
+    });
+
+    $(document).mouseup( function(e){ 
+		var div = $( ".select-init" );
+		if ( !div.is(e.target) 
+		    && div.has(e.target).length === 0 ) {
+			div.find(".list").hide(300);
+		}
+	});
+
+}
